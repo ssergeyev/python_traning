1ee60f4e4ff17989bbe60557bf02188755d2b577
s_sergeyev <sss999@yandex.kz>
2020-06-21 22:29:02 +0600

    run test

--- test_add_contact.py
+++ test_add_contact.py
@@ -5,49 +5,39 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
-class UntitledTestCase(unittest.TestCase):
+class test_add_contact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
     
-    def test_untitled_test_case(self):
-        wd = self.wd
-        wd.get("http://192.168.1.63/addressbook/index.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+
+    def add_new_contact(self, wd):
         wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(u"Любимов")
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
         wd.find_element_by_name("middlename").send_keys(u"Николаевич")
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys(u"Любимов")
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(u"Сергей")
-        wd.find_element_by_name("bday").click()
         Select(wd.find_element_by_name("bday")).select_by_visible_text("3")
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bmonth").click()
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text("October")
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1969")
         wd.find_element_by_name("new_group").click()
         Select(wd.find_element_by_name("new_group")).select_by_visible_text("family")
-        wd.find_element_by_name("new_group").click()
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         wd.find_element_by_link_text("home page").click()
         wd.find_element_by_link_text("Logout").click()
-    
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").send_keys("%s" % username)
+        wd.find_element_by_name("pass").send_keys("%s" % password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def test_untitled_test_case(self):
+        wd = self.wd
+        wd.get("http://192.168.1.63/addressbook/index.php")
+        self.login(wd, username="admin", password="secret")
+        self.add_new_contact(wd)
+
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
